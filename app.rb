f96322d462579cf45c795d73b8c08de6ca6ced91
Dana Weiss and Susha Dore <susha@>
2017-05-11 10:51:27 -0700

    add update and delete

--- app.rb
+++ app.rb
@@ -12,3 +12,20 @@ post '/categories' do
   Category.create(:title => params['title'])
   redirect '/'
 end
+
+get '/category/:id' do
+  @category = Category.find(params['id'].to_i)
+  erb :category
+end
+
+patch '/category/:id' do
+  @category = Category.find(params['id'].to_i)
+  @category.update(:title => params['title'])
+  erb :category
+end
+
+delete '/category/:id' do
+  category = Category.find(params['id'].to_i)
+  category.delete
+  redirect '/'
+end
