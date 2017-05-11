67ea054e8c6351eb16937abbc68b03a85271cd85
Dana Weiss and Susha Dore <dana@>
2017-05-11 10:05:22 -0700

    create functional callback

--- lib/category.rb
+++ lib/category.rb
@@ -1,3 +1,10 @@
 class Category < ActiveRecord::Base
- has_and_belongs_to_many(:recipes)
+  has_and_belongs_to_many :recipes
+  before_save :uppercase
+
+  private
+
+  def uppercase
+      self.title = title.upcase
+    end
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Category do
   it { should have_and_belong_to_many(:recipes)}
+
+  describe '#uppercase' do
+    it 'capitalizes the category title' do
+      category = Category.create({:title => 'christmas cheer'})
+      expect(category.title).to eq('CHRISTMAS CHEER')
+    end
+  end
 end
