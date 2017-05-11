1e62c8144509058380d77d4ff26cab3009977fdb
Dana Weiss and Susha Dore <susha@>
2017-05-11 10:15:13 -0700

    add validation spec to Category

--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -3,10 +3,13 @@ require 'spec_helper'
 describe Category do
   it { should have_and_belong_to_many(:recipes)}
 
-  describe '#uppercase' do
-    it 'capitalizes the category title' do
-      category = Category.create({:title => 'christmas cheer'})
-      expect(category.title).to eq('CHRISTMAS CHEER')
-    end
+  it 'capitalizes the category title' do
+    category = Category.create(:title => 'christmas cheer')
+    expect(category.title).to eq('CHRISTMAS CHEER')
+  end
+
+  it 'validates the presence of a title' do
+    category = Category.create(:title => "")
+    expect(category.save).to eq(false)
   end
 end
