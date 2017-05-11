98ffd0ccc74cef10146db4ec9c7f9fc4a43d758c
Dana Weiss and Susha Dore <susha@>
2017-05-11 10:17:06 -0700

    add validation spec to Category

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base
-  has_and_belongs_to_many(:categories)
+  has_and_belongs_to_many :categories
+  validates :title, :presence => true
+  before_save :uppercase
+
+  private
+
+  def uppercase
+      self.title = title.upcase
+    end
+end
 end
